b37753746d5cfb3f439d3005596dea8d83e99662
Vkrtn <v.khqritknove.000@gmail.com>
2023-11-02 18:09:18 +0200

    done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const box = document.querySelector(".box");
 const btn = document.querySelector(".add");
 const del = document.querySelector(".remove");
-const defore = document.querySelector('.before')
-const after = document.querySelector('.after')
+const defore = document.querySelector(".before");
+const after = document.querySelector(".after");
 const selected = document.querySelector(".selected");
 let count = 1;
+let copyAfter =1
+let copyBefore =1
 
 btn.addEventListener("click", (el) => {
   const item = document.createElement("div");
@@ -12,40 +14,38 @@ btn.addEventListener("click", (el) => {
   item.innerHTML = count;
   count++;
   item.onclick = function () {
+    defore.addEventListener("click", () => {
+      const clonedNode = item.cloneNode(true);
+      clonedNode.innerText = `${this.innerText} -${copyBefore}c`;
+      if (this.classList.contains("active")) {
+        copyBefore++
+        this.before(clonedNode);
+        this.classList.remove("active");
+        clonedNode.classList.remove("active");
+      }
+    });
+
+    after.addEventListener("click", (el) => {
+      const clonedNode = item.cloneNode(true);
+      clonedNode.innerText = `${this.innerText} -${copyAfter}c`;
+      if (this.classList.contains("active")) {
+        copyAfter++
+        this.after(clonedNode);
+        this.classList.remove("active");
+        clonedNode.classList.remove("active");
+      }
+    });
 
     hideAll(this);
-    this.classList.add("active");
+    this.classList.toggle("active");
     selected.innerText = `Selected el: ${this.innerText}`;
 
-    del.addEventListener('click',el=>{
-       if(this.classList.contains('active'))this.classList.add('del')
-    })
-
-    defore.addEventListener('click',el=>{
-        const clonedNode = item.cloneNode(true);
-        clonedNode.innerText = `${this.innerText} -c`
-        if (this.classList.contains('active')){
-            this.before(clonedNode)
-            this.classList.remove('active')
-            clonedNode.classList.remove('active')
-        }})   
-
-     after.addEventListener('click',el=>{
-        const clonedNode = item.cloneNode(true);
-        clonedNode.innerText = `${this.innerText} -c`
-        if (this.classList.contains('active')){
-            this.after(clonedNode)
-            this.classList.remove('active');
-            clonedNode.classList.remove('active')
-        }
-    })
+    del.addEventListener("click", (el) => {
+      if (this.classList.contains("active")) this.classList.add("del");
+    });
   };
   box.append(item);
 });
-
-
-
-
 
 const hideAll = (eL) => {
   const items = document.querySelectorAll(".item");
@@ -53,4 +53,3 @@ const hideAll = (eL) => {
     if (el != eL) el.classList.remove("active");
   });
 };
-
